6483cdb256fac3a2fbd3714ef3028c22019bdbc6
yingliufilms <yingliufilms@gmail.com>
2022-11-14 14:06:50 -0500

    installed superjson and babel-plugin-superjson-next; resolved getServerSideProps to fetch artists on the server side when index page loads

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import GradientLayout from '../components/gradientLayout';
+import prisma from '../lib/prisma';
 
-const Home = () => {
+const Home = ({ artists }) => {
 	return (
 		<GradientLayout
 			color="red"
@@ -16,6 +17,14 @@ const Home = () => {
 			<div>home page</div>
 		</GradientLayout>
 	);
+};
+
+export const getServerSideProps = async () => {
+	const artists = await prisma.artist.findMany({});
+	// console.log(artists);
+	return {
+		props: { artists }
+	};
 };
 
 export default Home;
