e02061c957cfd5e881c33a5ceede1dd975ff7799
yingliufilms <yingliufilms@gmail.com>
2023-03-02 12:08:12 -0500

    added comments on lib and pages files

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -3,6 +3,6 @@ import AuthForm from '../components/authForm';
 const Signin = () => {
 	return <AuthForm mode="signin" />;
 };
-
+//variable created in order to opt out and conditionally render the <PlayerLayout> wrapper in _app.tsx
 Signin.authPage = true;
 export default Signin;
